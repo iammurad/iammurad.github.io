7ce08faa7ad38edc0de86c65de0479d63e52c898
LinuxAlien <murik8900@gmail.com>
2021-12-28 23:50:50 +0400

    initial changes

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector(".sidebar");
-const expandIcon = document.querySelector(".expand-icon");
-let row = document.getElementById("row");
-expandIcon.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
